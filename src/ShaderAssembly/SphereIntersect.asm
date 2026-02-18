50a68be21be4f39f4239b1f973451c2ded1ed81f
Ben-Bingham <ben@benbingham.ca>
2026-02-17 17:52:30 -0700

    Partial implementation in assembly

--- src/ShaderAssembly/SphereIntersect.asm
+++ src/ShaderAssembly/SphereIntersect.asm
@@ -1,172 +1,195 @@
-// Compute uv, store it in v0
-s0 = registers[reg_pc][int(floor(z))];
-s1 = 1.0;
-s1 = registers[reg_pc][int(floor(s1))];
-v0[int(floor(z))] = s0;
-s2 = 1.0;
-v0[int(floor(s2))] = s1;
-s2 = 2.0;
-v0[int(floor(s2))] = z;
-s2 = 3.0;
-v0[int(floor(s2))] = z;
-	
-s0 = registers[reg_s][int(floor(z))];
-s5 = 1.0;
-s1 = registers[reg_s][int(floor(s5))];
-s0 = 1.0 / s0;
-s1 = 1.0 / s1;
-	
-s2 = v0[int(floor(z))];
-s5 = 1.0;
-s3 = v0[int(floor(s5))];
-s0 = s2 * s0;
-s1 = s3 * s1;
-v0[int(floor(z))] = s0;
-s5 = 1.0;
-v0[int(floor(s5))] = s1;
-	
-// Adjust the uv to center around the origin
-// Multiply by 2.0
-s0 = v0[int(floor(z))];
-s5 = 1.0;
-s1 = v0[int(floor(s5))];
-s2 = 2.0;
-s3 = s0 * s2;
-s4 = s1 * s2;
-v0[int(floor(z))] = s3;
-s5 = 1.0;
-v0[int(floor(s5))] = s4;
-	
-// Subtract 1.0
-s0 = v0[int(floor(z))];
-s5 = 1.0;
-s1 = v0[int(floor(s5))];
-s2 = 1.0;
-s2 = -s2;
-s0 = s0 + s2;
-s1 = s1 + s2;
-v0[int(floor(z))] = s0;
-s5 = 1.0;
-v0[int(floor(s5))] = s1;
-	
-// Adjust for aspect ratio
-s5 = 1.0;
-s0 = registers[reg_s][int(floor(s5))];
-s0 = 1.0 / s0;
-s1 = registers[reg_s][int(floor(z))];
-s0 = s1 * s0;
-s1 = v0[int(floor(z))];
-s0 = s1 * s0;
-v0[int(floor(z))] = s0;
-	
-// Initialize the ray direction, reuse the uv as the ray direction
-s5 = 2.0;
-v0[int(floor(s5))] = -1.0;
-s5 = 3.0;
-v0[int(floor(s5))] = z;
-	
-// Define sphere, s0 is the z coordinate
-s0 = 2.0;
-s0 = -s0;
-	
-// Compute a (from quadratic formula), stored in s1
-s1 = v0[int(floor(z))];
-s1 = s1 * s1;
-s5 = 1.0;
-s2 = v0[int(floor(s5))];
-s2 = s2 * s2;
-s5 = 2.0;
-s3 = v0[int(floor(s5))];
-s3 = s3 * s3;
-s1 = s1 + s2;
-s1 = s1 + s3;
-	
-// Compute b (from quadratic formula), stored in s2
-s2 = 2.0;
-s2 = -s2;
-s5 = 2.0;
-s3 = v0[int(floor(s5))];
-s2 = s2 * s3;
-s2 = s2 * s0;
-	
-// Compute c (from quadratic formula), stored in s3
-s3 = 1.0;
-s3 = -s3;
-s4 = s0 * s0;
-s3 = s4 + s3;
-	
-// Compute discriminant, store in s0
-s4 = s2 * s2;
-s5 = 4.0;
-s5 = s5 * s1;
-s5 = s5 * s3;
-s5 = -s5;
-s0 = s4 + s5;
-	
-// Using: max(a, b) = 0.5 * (a + b + |a - b|)
-	
-// Clamp discriminant first time
-// Set s1 = max(s0, 0.0)
-s1 = sign(s0);
-s1 = s1 * s0;
-s1 = s1 + s0;
-s2 = 2.0;
-s2 = 1.0 / 2.0;
-s1 = s2 * s1;
-	
-// Set s2 = min(s1, 1.0) = -max(-s1, -1.0)
-//s0 = -max(-s1, -1.0);
-s4 = 1.0;
-s1 = -s1;
-s3 = s4 + s1;
-s2 = sign(s3);
-s2 = s2 * s3;
-s4 = -s4;
-s3 = s4 + s2;
-s3 = s3 + s1;
-s0 = s3;
-s0 = -s0;
-s1 = 2.0;
-s1 = 1.0 / s1;
-s0 = s0 * s1;
-	
-// Take all values greater then 0, and ensure they are greater then 1
-s1 = 100.0;
-s0 = s0 * s1;
-	
-// Clamp discriminant second time
-// Set s1 = max(s0, 0.0)
-s1 = sign(s0);
-s1 = s1 * s0;
-s1 = s1 + s0;
-s2 = 2.0;
-s2 = 1.0 / 2.0;
-s1 = s2 * s1;
-	
-// Set s2 = min(s1, 1.0) = -max(-s1, -1.0)
-//s0 = -max(-s1, -1.0);
-s4 = 1.0;
-s1 = -s1;
-s3 = s4 + s1;
-s2 = sign(s3);
-s2 = s2 * s3;
-s4 = -s4;
-s3 = s4 + s2;
-s3 = s3 + s1;
-s0 = s3;
-s0 = -s0;
-s1 = 2.0;
-s1 = 1.0 / s1;
-s0 = s0 * s1;
-	
-s1 = 1.0;
-s2 = 2.0;
-s3 = 3.0;
+# Compute uv, store it in v0
+getComponent(pc, s0, z)		# s0 = pc.x
+load(s5, 1)					# s5 = 1.0
+getComponent(pc, s1, s5)	# s1 = pc.y
+setComponent(v0, s0, z)		# v0.x = s0
+load(s2, 1)					# s2 = 1.0
+setComponent(v0, s1, s2)	# v0.y = s1
+load(s2, 2)					# s2 = 2.0
+setComponent(v0, z, s2)		# v0.z = z
+load(s2, 3)					# s2 = 3.0
+setComponent(v0, z, s2)		# v0.w = z
 
-outFragColor[int(floor(z))] = s0;
-outFragColor[int(floor(s1))] = s0;
-outFragColor[int(floor(s2))] = s0;
-outFragColor[int(floor(s3))] = 1.0;
+getComponent(s, s0, z)		# s0 = s.x
+load(s5, 1)					# s5 = 1.0
+getComponent(s, s1, s5)		# s1 = s.y
+reciprocal(s0, s0)			# s0 = 1.0 / s0
+reciprocal(s1, s1)			# s1 = 1.0 / s1
+
+getComponent(v0, s2, z)		# s2 = v0.x
+load(s5, 1)					# s5 = 1.0
+getComponent(v0, s3, s5)	# s3 = v0.y
+multiply(s2, s0, s0)		# s0 = s2 * s0
+multiply(s3, s1, s1)		# s1 = s3 * s1
+setComponent(v0, s0, z)		# v0.x = s0
+load(s5, 1)					# s5 = 1.0
+setComponent(v0, s1, s5)	# v0.y = s1
+
+# Adjust the uv to center around the origin
+# Multiply by 2.0
+getComponent(v0, s0, z)		# s0 = v0.x
+load(s5, 1)					# s5 = 1.0
+getComponent(v0, s1, s5)	# s1 = v0.y
+load(s2, 2)					# s2 = 2.0
+multiply(s0, s2, s3)		# s3 = s0 * s2
+multiply(s1, s2, s4)		# s4 = s1 * s2
+setComponent(v0, s3, z)		# v0.x = s3
+load(s5, 1)					# s5 = 1.0
+setComponent(v0, s4, s5)	# v0.y = s4
+	
+## Subtract 1.0
+getComponent(v0, s0, z)		# s0 = v0.x
+load(s5, 1)					# s5 = 1.0
+getComponent(v0, s1, s5)	# s1 = v0.y
+load(s2, 1)					# s2 = 1.0
+negate(s2, s2)				# s2 = -s2
+add(s0, s2, s0)				# s0 = s0 + s2
+add(s1, s2, s1)				# s1 = s1 + s2
+setComponent(v0, s0, z)		# v0.x = s0
+load(s5, 1)					# s5 = 1.0
+setComponent(v0, s1, s5)	# v0.y = s1
+
+
+
+
+############### START DEBUG PRINTING
+getComponent(v0, s0, z)
+setComponent(c, s0, z)
+
+load(s5, 1)
+
+getComponent(v0, s0, s5)
+setComponent(c, s0, s5)
+
+############### END DEBUG PRINTING
+
+
+
+
+
+
+
+
+
+
+
+
+## Adjust for aspect ratio
+#load(s5, 1) # s5 = 1.0
+#s0 = registers[reg_s][int(floor(s5))];
+#s0 = 1.0 / s0;
+#s1 = registers[reg_s][int(floor(z))];
+#s0 = s1 * s0;
+#s1 = v0[int(floor(z))];
+#s0 = s1 * s0;
+#v0[int(floor(z))] = s0;
+#	
+## Initialize the ray direction, reuse the uv as the ray direction
+#s5 = 2.0;
+#v0[int(floor(s5))] = -1.0;
+#s5 = 3.0;
+#v0[int(floor(s5))] = z;
+#	
+## Define sphere, s0 is the z coordinate
+#s0 = 2.0;
+#s0 = -s0;
+#	
+## Compute a (from quadratic formula), stored in s1
+#s1 = v0[int(floor(z))];
+#s1 = s1 * s1;
+#load(s5, 1) # s5 = 1.0
+#s2 = v0[int(floor(s5))];
+#s2 = s2 * s2;
+#s5 = 2.0;
+#s3 = v0[int(floor(s5))];
+#s3 = s3 * s3;
+#s1 = s1 + s2;
+#s1 = s1 + s3;
+#	
+## Compute b (from quadratic formula), stored in s2
+#s2 = 2.0;
+#s2 = -s2;
+#s5 = 2.0;
+#s3 = v0[int(floor(s5))];
+#s2 = s2 * s3;
+#s2 = s2 * s0;
+#	
+## Compute c (from quadratic formula), stored in s3
+#s3 = 1.0;
+#s3 = -s3;
+#s4 = s0 * s0;
+#s3 = s4 + s3;
+#	
+## Compute discriminant, store in s0
+#s4 = s2 * s2;
+#s5 = 4.0;
+#s5 = s5 * s1;
+#s5 = s5 * s3;
+#s5 = -s5;
+#s0 = s4 + s5;
+#	
+## Using: max(a, b) = 0.5 * (a + b + |a - b|)
+#	
+## Clamp discriminant first time
+## Set s1 = max(s0, 0.0)
+#s1 = sign(s0);
+#s1 = s1 * s0;
+#s1 = s1 + s0;
+#s2 = 2.0;
+#s2 = 1.0 / 2.0;
+#s1 = s2 * s1;
+#	
+## Set s2 = min(s1, 1.0) = -max(-s1, -1.0)
+#s4 = 1.0;
+#s1 = -s1;
+#s3 = s4 + s1;
+#s2 = sign(s3);
+#s2 = s2 * s3;
+#s4 = -s4;
+#s3 = s4 + s2;
+#s3 = s3 + s1;
+#s0 = s3;
+#s0 = -s0;
+#s1 = 2.0;
+#s1 = 1.0 / s1;
+#s0 = s0 * s1;
+#	
+## Take all values greater then 0, and ensure they are greater then 1
+#s1 = 100.0;
+#s0 = s0 * s1;
+#	
+## Clamp discriminant second time
+## Set s1 = max(s0, 0.0)
+#s1 = sign(s0);
+#s1 = s1 * s0;
+#s1 = s1 + s0;
+#s2 = 2.0;
+#s2 = 1.0 / 2.0;
+#s1 = s2 * s1;
+#	
+## Set s2 = min(s1, 1.0) = -max(-s1, -1.0)
+#s4 = 1.0;
+#s1 = -s1;
+#s3 = s4 + s1;
+#s2 = sign(s3);
+#s2 = s2 * s3;
+#s4 = -s4;
+#s3 = s4 + s2;
+#s3 = s3 + s1;
+#s0 = s3;
+#s0 = -s0;
+#s1 = 2.0;
+#s1 = 1.0 / s1;
+#s0 = s0 * s1;
+#	
+#load(s1, 1) # s1 = 1.0
+#s2 = 2.0;
+#s3 = 3.0;
+#
+#outFragColor[int(floor(z))] = s0;
+#outFragColor[int(floor(s1))] = s0;
+#outFragColor[int(floor(s2))] = s0;
+#outFragColor[int(floor(s3))] = 1.0;
 
 
 ######################################################################################
